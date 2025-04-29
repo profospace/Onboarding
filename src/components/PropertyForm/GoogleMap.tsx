8fe3b96d132968a9ad0c802f21a14012725b566b
Anurag <anuragsonkar053@gmail.com>
2025-04-29 18:18:12 +0530

    google-map-key

--- src/components/PropertyForm/GoogleMap.tsx
+++ src/components/PropertyForm/GoogleMap.tsx
@@ -25,7 +25,7 @@ const GoogleMap: React.FC<GoogleMapProps> = ({
         }
 
         const script = document.createElement('script');
-        script.src = `https://maps.googleapis.com/maps/api/js?key=${import.meta.env.VITE_GOOGLE_MAPS_API_KEY}&libraries=places`;
+        script.src = `https://maps.googleapis.com/maps/api/js?key=${import.meta.env.VITE_GOOGLE_API_KEY}&libraries=places`;
         script.async = true;
         script.defer = true;
         script.onload = () => setIsLoaded(true);
