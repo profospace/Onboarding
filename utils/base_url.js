876cf4e2a8fc7dc709d086015c9fb13087f71381
Anurag <anuragsonkar053@gmail.com>
2025-04-30 11:16:14 +0530

    base_url

--- utils/base_url.js
+++ utils/base_url.js
@@ -1,4 +1,4 @@
-// export const base_url = "https://propertify.onrender.com"
-// export const auth_base_url = "https://propertify.onrender.com"
-export const base_url = "http://localhost:5029"
-export const auth_base_url = "http://localhost:5029"
+export const base_url = "https://propertify.onrender.com"
+export const auth_base_url = "https://propertify.onrender.com"
+// export const base_url = "http://localhost:5029"
+// export const auth_base_url = "http://localhost:5029"
