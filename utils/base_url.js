dfa6186f47f10bca277b151021e386e62dbe1479
Anurag <anuragsonkar053@gmail.com>
2025-05-10 18:00:26 +0530

    salesman-view-edit-own-leads

--- utils/base_url.js
+++ utils/base_url.js
@@ -2,3 +2,6 @@ export const base_url = "https://propertify.onrender.com"
 export const auth_base_url = "https://propertify.onrender.com"
 // export const base_url = "http://localhost:5029"
 // export const auth_base_url = "http://localhost:5029"
+
+// deadpool2569 - iy7J59VT9i   - local salesman
+// micky1198 - bc9ukakJMd   - local salesman
