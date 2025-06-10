1ce4b0ea5d786f2b17f63b8222e2c95f36c502bc
Anurag <anuragsonkar053@gmail.com>
2025-06-10 19:08:27 +0530

    base_url

--- utils/base_url.js
+++ utils/base_url.js
@@ -1,7 +1,9 @@
-export const base_url = "https://propertify.onrender.com"
-export const auth_base_url = "https://propertify.onrender.com"
+// export const base_url = "https://propertify.onrender.com"
+// export const auth_base_url = "https://propertify.onrender.com"
 // export const base_url = "http://localhost:5029"
 // export const auth_base_url = "http://localhost:5029"
+export const base_url = import.meta.env.VITE_BASE_URL
+export const auth_base_url = import.meta.env.VITE_BASE_URL
 
 // deadpool2569 - iy7J59VT9i   - local salesman
 // micky1198 - bc9ukakJMd   - local salesman
